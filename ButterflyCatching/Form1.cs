99deceea9af4764a834b03c5616e37c6c609dfee
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-05 10:54:09 -0300

    Nuevo método agregado al proyecto.

--- ButterflyCatching/Form1.cs
+++ ButterflyCatching/Form1.cs
@@ -6,5 +6,13 @@ namespace ButterflyCatching
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Método que permite inicializar el juego completo de la interfaz.
+
+            Form1 juego = new Form1(); // Para el juego completo en general.
+            juego.Show(); // Muestra la ejecución de la interfaz del juego.
+        }
     }
 }
