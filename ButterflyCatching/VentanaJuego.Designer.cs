3506525ba1de76be181137aaf04d113a27be163a
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-10 11:53:05 -0300

    Se creó una clase + la interfaz al juego.

--- ButterflyCatching/VentanaJuego.Designer.cs
+++ ButterflyCatching/VentanaJuego.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "VentanaJuego";
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            lblTemporizador = new Label();
+            lblCantMariposas = new Label();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(-4, 397);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(805, 54);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lblTemporizador
+            // 
+            lblTemporizador.AutoSize = true;
+            lblTemporizador.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTemporizador.Location = new Point(12, 403);
+            lblTemporizador.Name = "lblTemporizador";
+            lblTemporizador.Size = new Size(279, 38);
+            lblTemporizador.TabIndex = 1;
+            lblTemporizador.Text = "Tiempo restante: 00";
+            // 
+            // lblCantMariposas
+            // 
+            lblCantMariposas.AutoSize = true;
+            lblCantMariposas.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCantMariposas.Location = new Point(573, 403);
+            lblCantMariposas.Name = "lblCantMariposas";
+            lblCantMariposas.Size = new Size(181, 38);
+            lblCantMariposas.TabIndex = 2;
+            lblCantMariposas.Text = "Atrapadas: 0";
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += EventoTemporizador;
+            // 
+            // VentanaJuego
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.IndianRed;
+            BackgroundImage = Properties.Resources.background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblCantMariposas);
+            Controls.Add(lblTemporizador);
+            Controls.Add(pictureBox1);
+            DoubleBuffered = true;
+            Name = "VentanaJuego";
+            ShowIcon = false;
+            Text = "VentanaJuego";
+            Click += HacerClickEvento;
+            Paint += EventoPaint;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblTemporizador;
+        private Label lblCantMariposas;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
--- ButterflyCatching/VentanaJuego.cs
+++ ButterflyCatching/VentanaJuego.cs
@@ -16,5 +16,54 @@ namespace ButterflyCatching
         {
             InitializeComponent();
         }
+
+        // Evento para el temporizador del juego.
+
+        private void EventoTemporizador(object sender, EventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Evento para hacer click a toda la ventana del juego.
+
+        private void HacerClickEvento(object sender, EventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Evento para los colores de la interfaz visual del juego.
+
+        private void EventoPaint(object sender, PaintEventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Método para crear una mariposa a partir de una imagen
+
+        private void CrearMariposa()
+        {
+            // EN INSTANTES...
+        }
+
+        // Soporte para la interfaz del juego.
+
+        private void OnFrameChangedHandler(object? sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        // Método para reiniciar el juego.
+
+        private void ReiniciarJuego()
+        {
+            // EN INSTANTES...
+        }
+
+        // Método para finalizar la partida.
+
+        private void JuegoTerminado()
+        {
+            // EN INSTANTES...
+        }
     }
 }
